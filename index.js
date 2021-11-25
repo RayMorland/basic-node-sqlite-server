dd970be9ed031ec5d6ee7854cd7aa55f4dc405fc
RayMorland <raymondmorland@gmail.com>
2021-11-24 20:48:55 -0800

    Work on storefront api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var http = require("http");
+var https = require("https");
 const sqlite3 = require("sqlite3").verbose();
 let db = new sqlite3.Database("./nodesql.db", (err) => {
   if (err) {
@@ -7,47 +8,91 @@ let db = new sqlite3.Database("./nodesql.db", (err) => {
   console.log("Connected to the in-memory SQlite database.");
 });
 
-function dbConnect() {
-  return new Promise((resolve, reject) => {
-    // var items = [1];
-    // db.serialize(function () {
-    //   var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+const options = {
+  hostname: "morlandsdevstore.myshopify.com",
+  port: 443,
+  path: "/api/graphql.json",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "X-Shopify-Storefront-Access-Token": "e3ad9b2133e50c72d673596d5004d14b",
+  },
+};
 
-    //   for (var i = 0; i < 10; i++) {
-    //     stmt.run("Ipsum " + i);
-    //   }
-    //   stmt.finalize();
-
-    db.all("SELECT * FROM lorem WHERE info = 'Ipsum 3'", function (err, row) {
-      db.close((err) => {
-        if (err) {
-          console.log(err);
+const data = new TextEncoder().encode(
+  JSON.stringify({
+    query: `query {
+    products (first: 5) {
+        edges {
+            node {
+                id
+                title
+                description
+            }
         }
-        console.log("Closed the database connection.");
-        return;
-      });
-      resolve(row);
-      //   });
-    });
+    }
+}`,
+  })
+);
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on("data", (d) => {
+    process.stdout.write(d);
   });
-}
-
-var server = http.createServer(async (req, res) => {
-  let items = [];
-
-  try {
-    let dbItems = await dbConnect();
-    items = [...items, dbItems];
-  } catch (err) {
-    console.log(err);
-  }
-
-  let html = "";
-  items.forEach((item) => {
-    html += html + " " + JSON.stringify(item);
-  });
-
-  res.end(html);
 });
 
-server.listen(3000);
+req.on("error", (error) => {
+  console.error(error);
+});
+
+req.write(data);
+req.end();
+
+// function dbConnect() {
+//   return new Promise((resolve, reject) => {
+// var items = [1];
+// db.serialize(function () {
+//   var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+
+//   for (var i = 0; i < 10; i++) {
+//     stmt.run("Ipsum " + i);
+//   }
+//   stmt.finalize();
+
+//     db.all("SELECT * FROM lorem WHERE info = 'Ipsum 3'", function (err, row) {
+//       db.close((err) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//         console.log("Closed the database connection.");
+//         return;
+//       });
+//       resolve(row);
+//         });
+//     });
+//   });
+// }
+
+// var server = http.createServer(async (req, res) => {
+//   let items = [];
+
+//   try {
+//     let dbItems = await dbConnect();
+//     items = [...items, dbItems];
+//   } catch (err) {
+//     console.log(err);
+//   }
+
+//   let html = "";
+//   items.forEach((item) => {
+//     html += html + " " + JSON.stringify(item);
+//   });
+
+//   res.end(html);
+// });
+
+// server.listen(3000);
+
+// req.end();
